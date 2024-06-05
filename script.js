2851a0f41cd62df5c3c7f4044b36621abecf0bb4
MABEL QUISPE OLIVERA <131488004+mabelolivera10@users.noreply.github.com>
2024-06-05 10:12:34 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const stack = document.querySelector(".stack");
+const cards = Array.from(stack.children)
+  .reverse()
+  .filter((child) => child.classList.contains("card"));
+
+cards.forEach((card) => stack.appendChild(card));
+
+function moveCard() {
+  const lastCard = stack.lastElementChild;
+  if (lastCard.classList.contains("card")) {
+    lastCard.classList.add("swap");
+
+    setTimeout(() => {
+      lastCard.classList.remove("swap");
+      stack.insertBefore(lastCard, stack.firstElementChild);
+    }, 1200);
+  }
+}
+
+stack.addEventListener("click", function (e) {
+  const card = e.target.closest(".card");
+  if (card && card === stack.lastElementChild) {
+    card.classList.add("swap");
+
+    setTimeout(() => {
+      card.classList.remove("swap");
+      stack.insertBefore(card, stack.firstElementChild);
+      resetAutoplay();
+    }, 1200);
+  }
+});
+
+let autoplayInterval = setInterval(moveCard, 4000);
+
+function resetAutoplay() {
+  clearInterval(autoplayInterval);
+  autoplayInterval = setInterval(moveCard, 4000);
+}
